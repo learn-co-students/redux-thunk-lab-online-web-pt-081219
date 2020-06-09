dc09f484348b4838b122b01d2fb3382b8902993e
Dalia <dalia.sawaya@flatironschool.com>
2020-06-09 17:10:34 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,70 @@
 import React, { Component } from 'react';
+import CatList from './CatList'
+import {connect} from 'react-redux'
+import { fetchCats } from './actions/catActions'
 
 class App extends Component {   
+
+  componentDidMount(){
+    this.props.fetchCats()
+  }
   
   render() {
     return (
       <div>
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        <CatList catPics={this.props.catPics} />
+
       </div>
     );
   }
 }
 
-export default App
+const mapDispatchToProps = state => {
+  return {
+    catPics: state.cats,
+    loading: state.loading
+  }
+}
+
+export default connect(mapDispatchToProps, { fetchCats })(App)
+
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import { fetchCats } from './actions/catActions'
+// import CatList from './CatList'
+
+// class App extends Component {   
+  
+//   componentDidMount() {
+//     this.props.fetchCats()
+//   }
+  
+//   handleLoading = () => {
+//     console.log(this.props.loading)
+//     if(this.props.loading) {
+//       return <div>Loading...</div>
+//     } else {
+//       return <CatList catPics={this.props.catPics} />
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>CatBook</h1>
+//         {this.handleLoading()}
+//       </div>
+//     );
+//   }
+// }
+
+// const mapDispatchToProps = state => {
+//   return {
+//     catPics: state.cats,
+//     loading: state.loading
+//   }
+// }
+
+// export default connect(mapDispatchToProps, { fetchCats })(App)
 
